7722fd0e278b6b83c92fb2a876906668327de8ac
Anshit Jain <amanjainaj8516@gmail.com>
2022-08-24 13:19:47 +0530

    this is first time

---
+++ test.py
@@ -0,0 +1,15 @@
+import pymongo
+
+
+client = pymongo.MongoClient('mongodb+srv://Amanjain12:Amanjain12@anshit.5aeevdi.mongodb.net/?retryWrites=true&w=majority')
+db = client.test
+
+print(db)
+d1 = {
+    "username": "Anshit",
+    "phone-number": "9559277726",
+    "age": "21"
+}
+db1 = client['mongo']
+coll = db1['test']
+coll.insert_one(d1)
